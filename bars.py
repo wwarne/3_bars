32913f6f74a3d1b812d0578e7c5405bcbe179d63
Dmitry Plevkov <wwarne@gmail.com>
2017-01-30 21:45:05 +0300

    Изменен способ расчёта расстояний между точками на более упрощённый.

--- bars.py
+++ bars.py
@@ -2,12 +2,6 @@ import json
 import math
 import sys
 
-# Из https://en.wikipedia.org/wiki/Earth_radius#Mean_radius.
-# Средний радиус земли, принимаемой за сферу,расчёты с таким приближением
-# могут привести к ошибке вычислений порядка 0,5%
-
-EARTH_RADIUS = 6371009  # в метрах
-
 
 def load_data(filepath):
     try:
@@ -41,33 +35,13 @@ def get_distance(latitude_point1, longitude_point1, latitude_point2, longitude_p
     :param longitude_point1: долгота первой точки
     :param latitude_point2: широта второй точки
     :param longitude_point2: долгота второй точки
-    :return: расстояние между точками в метрах
+    :return: расстояние между точками
 
-    Для расчёта расстояния используется сферическая теорема косинусов.
+    Для расчётов в пределах одного города буду использовать приблизительную модель - весь город находится на плоскости,
+    а широта и долгота - это координаты X и Y, и расстояние между ними считается простой формулой.
+    А так как выводить его не нужно, то и переводить в метры или километры - тоже.
     """
-    # Значения углов должны быть в радианах
-    p1_lat = math.radians(float(latitude_point1))
-    p1_lon = math.radians(float(longitude_point1))
-    p2_lat = math.radians(float(latitude_point2))
-    p2_lon = math.radians(float(longitude_point2))
-
-    longitude_delta = p2_lon - p1_lon
-
-    # Эти переменные я расчитываю чтобы не городить трёхэтажную вложенность в основной формуле
-    cos_l1 = math.cos(p1_lat)
-    cos_l2 = math.cos(p2_lat)
-    sin_l1 = math.sin(p1_lat)
-    sin_l2 = math.sin(p2_lat)
-    cos_delta = math.cos(longitude_delta)
-    sin_delta = math.sin(longitude_delta)
-    """
-    Формула для расчёта угловой разницы https://en.wikipedia.org/wiki/Great-circle_distance
-    atan2(y, x) используется вместо простой atan(y/x) т.к. atan2 возвращает результат с учётом четверти,
-    в которой находится точка (X, Y).И поэтому результат тоже оказывается в правильной четверти круга.
-    """
-    y = math.sqrt(math.pow(cos_l2 * sin_delta, 2) + math.pow(cos_l1 * sin_l2 - sin_l1 * cos_l2 * cos_delta, 2))
-    x = sin_l1 * sin_l2 + cos_l1 * cos_l2 * cos_delta
-    return math.atan2(y, x) * EARTH_RADIUS
+    return math.sqrt((latitude_point2 - latitude_point1) ** 2 + (longitude_point2 - longitude_point1) ** 2)
 
 
 def check_script_arguments():
